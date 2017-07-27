b3041ece997e51ce39d99ee10ac6290954e13761
benhayehudi <bengreenberg@gmail.com>
2017-07-27 19:30:40 -0400

    added controller functionality for admin edit and delete methods

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -15,12 +15,21 @@ class AdminController < ApplicationController
   end
 
   def update
+    current_parent.update(parent_params)
+    current_parent.save
+    redirect_to edit_admin_path(current_parent)
   end
 
   def destroy
+    current_parent.delete
+    redirect_to admin_index_path
   end
 
   private
+
+  def parent_params
+    params.require(:parent).permit(:name, :email)
+  end
 
   def is_admin?
     current_user.id == 1 || current.user.admin == true
