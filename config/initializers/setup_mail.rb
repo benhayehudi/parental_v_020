bb9799bc93fc3422485744061c8579b507a6908f
benhayehudi <bengreenberg@gmail.com>
2017-07-31 19:45:33 -0400

    working on reminder email functionality

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,11 +2,11 @@ ActionMailer::Base.smtp_settings = {
   :address              => "smtp.sparkpostmail.com",
   :port                 => 587,
   :user_name            => "SMTP_Injection",
-  :password             => Rails.application.secrets.api_key,
+  :password             => ENV['PARENTAL_MAIL_KEY']
   :authentication       => :login,
   :format               => "html",
   :enable_starttls_auto => true,
 
 }
-ActionMailer::Base.default_url_options[:host] = "localhost:3000"
+ActionMailer::Base.default_url_options[:host] = "parental.life"
 ActionMailer::Base.delivery_method = :smtp
